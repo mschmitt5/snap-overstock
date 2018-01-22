0bc68a1219d63d1555dd8a52cda30b7090f43a62
Mary <schmitt.mary7@gmail.com>
2018-01-22 08:35:06 -0700

    commit with attempt at bitcoin function.

--- bitcoin.php
+++ bitcoin.php
@@ -6,3 +6,19 @@
  * Time: 8:07 AM
  */
 
+function bitcoinWalletBalance() : float {
+    $oneBitcoin = 1;
+    $oneBitcoinCash = .22;
+    $oneBitcoin = $oneBitcoinCash;
+    $priceInBitcoin = 2*$oneBitcoin;
+    $bitcoinWalletBalance = 3*$oneBitcoinCash;
+    $newBitcoinWalletBalance = $bitcoinWalletBalance - $priceInBitcoin;
+    if ($priceInBitcoin <= $bitcoinWalletBalance) {
+        return ($newBitcoinWalletBalance);
+    } else {
+        return (false);
+    }
+}
+
+
+
